6a1733d1a6382384ce3a77859400892e20fd1060
国吉 真木朗 <drpepper_999@mkuniyoshi-mac.local>
2012-08-31 18:28:20 +0900

    Initial Commit

---
+++ circusboy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  circusboy
+//
+//  Created by 国吉 真木朗 on 12/08/31.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
